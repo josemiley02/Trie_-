4634d807485660f4f86c277e1d47e750340b1969
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-07-22 20:13:31 -0400

    NodeTrie

--- Node Trie/NodeTrie.cs
+++ Node Trie/NodeTrie.cs
@@ -1,18 +1,16 @@
-namespace Trie.Node
+namespace Trie.Node;
+public class NodeTrie
 {
-    public class NodeTrie
+    public NodeTrie(char vlaue)
     {
-        public NodeTrie(char vlaue)
-        {
-            Value = vlaue;
-            EndOfWord = false;
-            Children = [];
-        }
-
-        public char Value { get; private set; }
-        public bool EndOfWord { get; set; }
-        public Dictionary<char, NodeTrie> Children { get; private set;}
-        
-        public NodeTrie this[char key] => Children[key];
+        Value = vlaue;
+        EndOfWord = false;
+        Children = [];
     }
+
+    public char Value { get; private set; }
+    public bool EndOfWord { get; set; }
+    public Dictionary<char, NodeTrie> Children { get; private set; }
+
+    public NodeTrie this[char key] => Children[key];
 }
