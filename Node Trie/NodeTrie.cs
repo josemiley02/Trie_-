02485a3387ee58bd2cf463ee0fac53717c250932
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-07-22 21:04:38 -0400

    Adding invariante Deep in NodeTrie

--- Node Trie/NodeTrie.cs
+++ Node Trie/NodeTrie.cs
@@ -1,15 +1,17 @@
 namespace Trie.Node;
 public class NodeTrie
 {
-    public NodeTrie(char vlaue)
+    public NodeTrie(char vlaue, int deep)
     {
         Value = vlaue;
         EndOfWord = false;
         Children = [];
+        Deep = deep;
     }
 
     public char Value { get; private set; }
     public bool EndOfWord { get; set; }
+    public int Deep { get; private set; }
     public Dictionary<char, NodeTrie> Children { get; private set; }
 
     public NodeTrie this[char key] => Children[key];
